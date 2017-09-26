f7047daccfe28273a17536da7896dc6a07f46f0b
Nick Sevens <nick_sevens@hotmail.com>
2017-09-26 11:40:03 +0200

    Fixed / trimming

--- app/src/utils/Utils.ts
+++ app/src/utils/Utils.ts
@@ -1,3 +1,5 @@
+import * as trimend from "lodash.trimend";
+
 export class Utils {
   /*
    * Function to get Site Collection URL
@@ -53,7 +55,7 @@ export class Utils {
    */
   public static getLayoutsPageUrl(libraryName: string): string {
     if (_spPageContextInfo && _spPageContextInfo.webServerRelativeUrl) {
-      return _spPageContextInfo.webServerRelativeUrl + "/_layouts/15/" + libraryName;
+      return trimend(_spPageContextInfo.webServerRelativeUrl) + "/_layouts/15/" + libraryName;
     }
 
     if (window
